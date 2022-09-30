a658d58885015c67b6895394da0944294dcfe7f3
ramansaitm <raman_183039@saitm.org>
2022-10-01 00:55:31 +0530

    first commit

---
+++ Pattern.js
@@ -0,0 +1,6 @@
+
+const number = 5
+
+    for (let i = 1; i <= number; i++) {
+            console.log("* ".repeat(i));
+    }
---
+++ Prime.js
@@ -0,0 +1,26 @@
+
+const number = 5
+let isPrime = true;
+
+if (number === 1) {
+    console.log("1 is not a prime no");
+}
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number}: is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+else {
+    console.log("The number is not a prime number.");
+}
